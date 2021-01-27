17fa3c34e6df6822efd98fd06e3c1dd19611a09a
Dimitri <dimitri.despert@gmail.com>
2021-01-27 09:02:03 +0100

    BLOG V1.8 - Fonctionnel, affiche article et commentaire avec blogpost&id

--- app/persistences/blogPostData.php
+++ app/persistences/blogPostData.php
@@ -6,11 +6,11 @@
     }
 
     function blogPostById(PDO $isDB, $idPost) {
-        $resultPost = $isDB->query("SELECT title, text FROM posts WHERE id = $idPost");
-        return $resultPost ->fetch(PDO::FETCH_ASSOC);
+        $resultPost = $isDB->query("SELECT title, text, nickname FROM posts INNER JOIN authors ON posts.authors_id = authors.id  WHERE posts.id = $idPost");
+        return $resultPost->fetch(PDO::FETCH_ASSOC);
     }
 
     function commentsByBlogPost(PDO $isDB, $idPost) {
-        $resultPost = $isDB->query("SELECT text FROM comments INNER JOIN authors ON comments.authors_id = authors.id WHERE posts_id = $idPost");
-        return $resultPost ->fetch(PDO::FETCH_ASSOC);
+        $resultPost = $isDB->query("SELECT text, nickname FROM comments INNER JOIN authors ON comments.authors_id = authors.id WHERE posts_id = $idPost");
+        return $resultPost->fetch(PDO::FETCH_ASSOC);
     }
--- ressources/views/blogPost.tpl
+++ ressources/views/blogPost.tpl
@@ -1,7 +1,20 @@
 <?php
     include('ressources/views/header.tpl');
 
-    var_dump($isPost);
-    var_dump($isPostComments);
+    if (!empty($isPost)) {
+        foreach ($isPost as $row) {
+            echo "<li>" . $row . "</li>";
+        }
+    } else {
+        echo "Article inaccessible ou introuvable !";
+    }
+
+    if (!empty($isPost)) {
+        foreach ($isPostComments as $row) {
+        echo "<li>" . $row . "</li>";
+        }
+    } else {
+        echo "Article inaccessible ou introuvable !";
+    }
 
     include('ressources/views/footer.tpl');
